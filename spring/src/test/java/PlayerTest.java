5cf69565a2710aa74f3916f5eb4412e06f5e1ee4
Emile Generat <egenerat@users.noreply.github.com>
2020-05-07 18:08:59 +0100

    Spring: setup

---
+++ spring/src/test/java/PlayerTest.java
@@ -0,0 +1,27 @@
+import junit.framework.Test;
+import junit.framework.TestCase;
+import junit.framework.TestSuite;
+
+
+public class PlayerTest extends TestCase {
+    /**
+     * Create the test case
+     *
+     * @param testName
+     *          name of the test case
+     */
+    public PlayerTest(String testName) {
+        super(testName);
+    }
+
+    /**
+     * @return the suite of tests being tested
+     */
+    public static Test suite() {
+        return new TestSuite(PlayerTest.class);
+    }
+
+    public void testAlwaysTrue() {
+        assertTrue(true);
+    }
+}
