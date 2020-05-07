faa5b23641347a9ff93f434271753f272e10d4f3
Emile Generat <egenerat@users.noreply.github.com>
2020-05-07 20:32:48 +0100

    Spring: Promoted to Wood 2 League Mono pacman

--- spring/src/main/java/Player.java
+++ spring/src/main/java/Player.java
@@ -1,6 +1,42 @@
 import java.util.*;
-import java.io.*;
-import java.math.*;
+
+class Coord {
+    int x;
+    int y;
+
+    Coord(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    int getFlyingDistance(Coord other) {
+        return Math.abs(this.x - other.x) + Math.abs(this.y - other.y);
+    }
+
+    public String toString() {
+        return x + ", " + y;
+    }
+}
+
+class Pellet {
+    Coord pos;
+    int value;
+
+    Pellet(Coord c, int value) {
+        this.pos = c;
+        this.value = value;
+    }
+}
+
+class Pacman {
+    Coord pos;
+    int id;
+
+    Pacman(Coord pos, int id) {
+        this.pos = pos;
+        this.id = id;
+    }
+}
 
 class Player {
 
@@ -16,37 +52,69 @@ class Player {
             System.err.println(row);
         }
 
+        List<Pacman> myPacmans;
         // game loop
         while (true) {
+            myPacmans = new ArrayList<>();
             int myScore = in.nextInt();
             int opponentScore = in.nextInt();
             int visiblePacCount = in.nextInt(); // all your pacs and enemy pacs in sight
             for (int i = 0; i < visiblePacCount; i++) {
                 int pacId = in.nextInt(); // pac number (unique within a team)
                 boolean mine = in.nextInt() != 0; // true if this pac is yours
-                int x = in.nextInt(); // position in the grid
-                int y = in.nextInt(); // position in the grid
+                int x = in.nextInt();
+                int y = in.nextInt();
                 String typeId = in.next(); // unused in wood leagues
                 int speedTurnsLeft = in.nextInt(); // unused in wood leagues
                 int abilityCooldown = in.nextInt(); // unused in wood leagues
+
+                if (mine) {
+                    myPacmans.add(new Pacman(new Coord(x, y), pacId));
+                }
             }
             int visiblePelletCount = in.nextInt(); // all pellets in sight
-            int targetX = -1;
-            int targetY = -1;
+            Coord target = null;
+            List<Pellet> pellets10 = new ArrayList<>();
+            List<Pellet> pellets1 = new ArrayList<>();
             for (int i = 0; i < visiblePelletCount; i++) {
                 int x = in.nextInt();
                 int y = in.nextInt();
-                int value = in.nextInt(); // amount of points this pellet is worth
+                int value = in.nextInt();
                 if (value == 10) {
-                    targetX = x;
-                    targetY = y;
+                    pellets10.add(new Pellet(new Coord(x, y), value));
+                }
+                else {
+                    pellets1.add(new Pellet(new Coord(x, y), value));
                 }
             }
-            if (targetX >=0 && targetY >= 0) {
-                System.out.println("MOVE 0 " + targetX + " " + targetY); // MOVE <pacId> <x> <y>
+            Integer minDistance = null;
+            Pacman pacman = myPacmans.get(0);
+            System.err.println("My current position: " + pacman.pos.x + "," + pacman.pos.y);
+            System.err.println("Pellet 10: " + pellets10.size() + ", Pellets 1: " + pellets1.size());
+            for (Pellet p : pellets10) {
+                int tmpDist = p.pos.getFlyingDistance(pacman.pos);
+                System.err.println(p.pos + " distance: " + tmpDist);
+                if (minDistance == null || tmpDist < minDistance) {
+                    minDistance = tmpDist;
+                    target = p.pos;
+                }
+            }
+            if (target == null) {
+                for (Pellet p : pellets1) {
+                    int tmpDist = p.pos.getFlyingDistance(pacman.pos);
+                    System.err.println(p.pos + " distance: " + tmpDist);
+                    if (minDistance == null || tmpDist < minDistance) {
+                        minDistance = tmpDist;
+                        target = p.pos;
+                    }
+                }
+            }
+            System.err.println("Closest is : " + target + " with distance of: " + minDistance);
+            if (target != null) {
+                System.out.println("MOVE 0 " + target.x + " " + target.y);
             }
             else {
-                System.out.println("MOVE 0 0 0"); // MOVE <pacId> <x> <y>
+                System.out.println("MOVE 0 0 0");
             }
             
         }
